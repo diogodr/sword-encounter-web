07c56f7ba2c258dc5edc715e7a6c0d8792a26fdd
Diogo Rosa <diogo.duarte@corebiz.ag>
2021-12-07 19:12:53 -0300

    ajustando header do axios3

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
+import https from "https";
 
 const api = axios.create({
-  rejectUnauthorized: false,
   headers: {
     "Access-Control-Allow-Headers":
       "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
   },
+  httpAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
   baseURL: "http://diogoddr-001-site1.ctempurl.com/api",
 });
 
