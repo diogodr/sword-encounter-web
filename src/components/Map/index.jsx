284ed47b2930d3c51a64bc511eb8d33994a2c87c
Diogo Rosa <diogo.duarte@corebiz.ag>
2022-03-25 17:24:08 -0300

    Chamando mapa da api

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useCampaign } from "../../contexts/campaignContext";
 import api from "../../services/api";
 import fileApi from "../../services/fileApi";
+import mapDefault from "../../assets/map-white.png";
 
 import { Container, Mapa } from "./styles";
 
 function Map() {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [maps, setMaps] = useState([]);
+  const [selectedMap, setSelectedMap] = useState("");
   const contextCampaign = useCampaign();
 
   async function addMap() {
@@ -14,30 +17,36 @@ function Map() {
     formData.append("image", selectedFile);
 
     const response = await fileApi.post("/3/image", formData);
-    console.log("RESPONSE:", response.data);
 
     const body = contextCampaign.campaign;
     body.maps.push(response.data.data.link);
-    console.log(body);
-    const response2 = await api.put(
-      `/campaigns/${contextCampaign.campaign.id}`,
-      body
-    );
-    console.log(response2);
+
+    await api.put(`/campaigns/${contextCampaign.campaign.id}`, body);
   }
+
+  async function getGameController() {
+    const response = await api.get(`/game/${contextCampaign.campaign.id}`);
+    setMaps(response.data.campaign.maps);
+  }
+
+  useEffect(() => {
+    getGameController();
+  }, []);
 
   return (
     <Container>
       <div className="select-container">
         <div className="select">
-          <select id="standard-select">
-            <option value="d4">D4</option>
-            <option value="d6">D6</option>
-            <option value="d8">D8</option>
-            <option value="d10">D10</option>
-            <option value="d12">D12</option>
-            <option value="d20">D20</option>
-            <option value="d100">D100</option>
+          <select
+            id="standard-select"
+            value={selectedMap}
+            onChange={(e) => setSelectedMap(e.target.value)}
+          >
+            {maps.map((map, index) => (
+              <option key={map} value={map}>
+                Mapa {index}
+              </option>
+            ))}
           </select>
           <span className="focus"></span>
         </div>
@@ -48,10 +57,7 @@ function Map() {
         />
         <button onClick={addMap}>Adicionar mapa</button>
       </div>
-      <Mapa
-        src="http://3.bp.blogspot.com/-h-OAO9JIMCA/U6SdPf1i8SI/AAAAAAAAAYA/MmCss-Udgig/s1600/10457884_10152470231476071_3391610058143746742_n.jpg"
-        alt="mapa"
-      />
+      <Mapa src={selectedMap ? selectedMap : mapDefault} alt="mapa" />
     </Container>
   );
 }
